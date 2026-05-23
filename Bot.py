2ce09a0e7f943f791cd664320b6c43cbcb3ae617
farsgallery <alireza.heydarinejad@gmail.com>
2026-05-24 00:42:14 +0330

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,9 +1,6 @@
-
-import os
 import logging
-from flask import Flask, request
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext, ConversationHandler, CallbackQueryHandler
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext, ConversationHandler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -12,8 +9,6 @@ MIN_HEIGHT = {"shid": 2.0, "zara": 1.5, "karkareh": 0}
 MIN_AREA = {"shid": 2.0, "zara": 1.5, "karkareh": 1.5}
 
 SELECTING_TYPE, ENTERING_WIDTH, ENTERING_HEIGHT = range(3)
-
-app = Flask(__name__)
 
 async def start(update: Update, context: CallbackContext):
     keyboard = [
@@ -95,8 +90,4 @@ async def calculate_price(update: Update, context: CallbackContext):
     await update.message.reply_text("میخوای دوباره حساب کنی؟", reply_markup=reply_markup)
     return ConversationHandler.END
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-
-builder = Application.builder()
-builder = builder.token(BOT_TOKEN)
-application = builder.build()...
+async …
