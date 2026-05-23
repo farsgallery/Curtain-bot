0ac01f8f13d2462e25c02d298d21feafa2923bd5
farsgallery <alireza.heydarinejad@gmail.com>
2026-05-24 00:20:03 +0330

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -97,5 +97,3 @@ async def calculate_price(update: Update, context: CallbackContext):
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 application = Application.builder().token(BOT_TOKEN).build()
-
-conv_handler = ConversationHandler…
