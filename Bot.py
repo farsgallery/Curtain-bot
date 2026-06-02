b359b5c6502e47de3d0cbe0c22ac73e1b3224f6f
farsgallery <alireza.heydarinejad@gmail.com>
2026-06-02 22:32:29 +0330

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -8,10 +8,6 @@ from telegram import (
     ReplyKeyboardMarkup,
 )
 
-from telegram.error import BadRequest
-
-CHANNEL_USERNAME = '@irandecoration_gallery'
-
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -27,7 +23,7 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-TOKEN = "8737297309:AAFBV78gJvC4ilHAAZ0pQS6hVsD7KL5_guQ"
+TOKEN = "8737297309:AAFEl8XdfWGQb_iNYjuSjido1Tgeo2XL-hA"
 
 # ---------------- تنظیمات پرده ----------------
 
@@ -89,41 +85,9 @@ reply_menu = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
-
-async def is_member(user_id, context):
-    try:
-        member = await context.bot.get_chat_member(CHANNEL_USERNAME, user_id)
-        return member.status in ["creator","administrator","member"]
-    except Exception:
-        return False
-
-async def force_join(update, context):
-    keyboard=[
-        [InlineKeyboardButton("📢 عضویت در کانال", url=f"https://t.me/{CHANNEL_USERNAME.replace('@','')}")],
-        [InlineKeyboardButton("✅ تایید عضویت", callback_data="check_join")]
-    ]
-    msg="❌ برای استفاده از ربات ابتدا عضو کانال شوید."
-    if update.message:
-        await update.message.reply_text(msg, reply_markup=InlineKeyboardMarkup(keyboard))
-    else:
-        await update.callback_query.message.reply_text(msg, reply_markup=InlineKeyboardMarkup(keyboard))
-
-async def check_join(update, context):
-    query=update.callback_query
-    await query.answer()
-    if await is_member(query.from_user.id, context):
-        await start(update, context)
-    else:
-        await query.message.reply_text("❌ هنوز عضو کانال نشده‌اید.")
-
-
 # ---------------- استارت ----------------
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    if not await is_member(update.effective_user.id, context):
-        await force_join(update, context)
-        return ConversationHandler.END
 
     text = """
 🎨 به ربات مجموعه هُنری فــارس گـالری خوش آمدید
@@ -670,8 +634,6 @@ def main():
             pattern="^back_start$"
         )
     )
-
-    app.add_handler(CallbackQueryHandler(check_join, pattern="^check_join$"))
 
     print("✅ Bot is running...")
 
