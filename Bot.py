ea865598a71d9f3a3baee2396240122630f491cd
farsgallery <alireza.heydarinejad@gmail.com>
2026-06-02 20:56:06 +0330

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -23,7 +23,7 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-TOKEN = "8737297309:AAFBV78gJvC4ilHAAZ0pQS6hVsD7KL5_guQ"
+TOKEN = "8737297309:AAFEl8XdfWGQb_iNYjuSjido1Tgeo2XL-hA"
 
 # ---------------- تنظیمات پرده ----------------
 
