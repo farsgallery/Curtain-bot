7f86e19d7c3b27f4606efecb70144a9c8008689b
farsgallery <alireza.heydarinejad@gmail.com>
2026-06-09 21:18:47 +0330

    Create bale_bot.py

---
+++ bale_bot.py
@@ -0,0 +1,57 @@
+from bale import Bot, Message, CallbackQuery
+from bale import InlineKeyboardMarkup, InlineKeyboardButton
+from bale.handlers import CommandHandler, CallbackQueryHandler
+from bale.checks import Data
+
+TOKEN = "1707286533:BRQqnArmEadLXFq7kZMaStVZXg6PkCsiQWg"
+
+bot = Bot(TOKEN)
+
+
+@bot.handle(CommandHandler("start"))
+async def start(message: Message):
+
+    keyboard = InlineKeyboardMarkup()
+
+    keyboard.add(
+        InlineKeyboardButton(
+            text="💰 استعلام قیمت",
+            callback_data="price"
+        )
+    )
+
+    keyboard.add(
+        InlineKeyboardButton(
+            text="🛒 خرید",
+            callback_data="buy"
+        ),
+        row=2
+    )
+
+    await message.reply(
+        "🎨 به ربات فارس گالری خوش آمدید\n\nیکی از گزینه‌ها را انتخاب کنید:",
+        components=keyboard
+    )
+
+
+@bot.handle(CallbackQueryHandler(Data("price")))
+async def price(callback: CallbackQuery):
+
+    await callback.answer("استعلام قیمت")
+
+    await callback.message.reply(
+        "💰 بخش استعلام قیمت\n\nاینجا بعداً انتخاب پرده و محاسبه قیمت را اضافه می‌کنیم."
+    )
+
+
+@bot.handle(CallbackQueryHandler(Data("buy")))
+async def buy(callback: CallbackQuery):
+
+    await callback.answer("خرید")
+
+    await callback.message.reply(
+        "🛒 لینک خرید:\nhttps://farsgallery.com"
+    )
+
+
+bot.run()
